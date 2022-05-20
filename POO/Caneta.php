f62529e81d535d4c5c07bdf765e202bad7236cf2
joaocorner <joaoeduardocorner@gmail.com>
2022-05-20 13:30:02 -0300

    first commit

---
+++ POO/Caneta.php
@@ -0,0 +1,19 @@
+<?php
+class Caneta
+{
+  var $modelo;
+  var $cor;
+  var $ponta;
+  var $carga;
+  var $tampada;
+
+  function rabiscar()
+  {
+  }
+  function tampar()
+  {
+  }
+  function destampar()
+  {
+  }
+}
---
+++ POO/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Aula 02 - POO</title>
+</head>
+
+<body>
+  <?php
+  require_once 'Caneta.php';
+  $c1 = new Caneta;
+  $c1->cor = "Azul";
+  $c1->ponta = 0.5;
+  $c1->tampada = true; //falso mostra vazio, verdadeiro mostra 1
+
+  var_dump($c1); //para mostrar o estado em PHP
+  print_r($c1); //outro jeito de mostrar o estado do objeto
+  ?>
+</body>
+
+</html>
