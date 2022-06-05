1af59ad1f955a16c0baef113e0bdc02bdb7ff4e5
joaocorner <joaoeduardocorner@gmail.com>
2022-06-04 22:34:31 -0300

    exercício 1 pegando valor de formulário

--- PHP/aula6/01valor.php
+++ PHP/aula6/01valor.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,9 +8,19 @@
     <link rel="stylesheet" href="../_css/estilo.css">
     <title>MODELO</title>
 </head>
+
 <body>
     <div>
-        
+        <?php
+        $valor = $_GET["v"];
+        echo "O valor enviado foi $valor";
+        echo "</br>A raiz é  " . number_format(sqrt($valor),2);
+        $rq = sqrt($valor);
+        ?>
+        <br><br>
+        <a href="01exercicio.html">Voltar</a>
     </div>
 </body>
+
 </html>
+
